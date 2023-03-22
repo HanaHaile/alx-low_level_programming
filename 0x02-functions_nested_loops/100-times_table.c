e48e2aab34c0ea66bf12b8c2126d90bfefcedcb5
HanaHaile <hanahaileg2002@gmail.com>
2023-03-21 19:28:01 -0700

    minor fix

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,11 +8,12 @@
 void print_times_table(int n)
 {
 	int i, j;
-
+        if (n==0) _putchar('0');
 	if (n > 0 && n < 15)
 	{
 		for (i = 0; i <= n; i++)
 		{
+     
 			_putchar('0');
 			for (j = 1; j <= n; j++)
 				putformat(i * j);
