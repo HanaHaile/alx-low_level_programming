9c8f63b56f1b90664c26b56144924826bd0e5cca
HanaHaile <hanahaileg2002@gmail.com>
2023-04-07 11:46:11 -0700

    what's my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * man - prints the name of the program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 9success)
+ */
+int main(int argc _arttribute__((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
+
+	return (0);
+}
